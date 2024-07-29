948860d01706979de43afb4394f1bfe7e878765c
Kevin2024-git <kevin.zambrano@tecnologicoliceocristiano.edu.ec>
2024-07-29 17:50:59 -0500

    06. Datos_T.py

---
+++ 06. Datos_T.py
@@ -0,0 +1,26 @@
+# Write code below 💖
+
+name = 'Kevin Zambrano'
+user_id = 80339887
+progress = 9.0
+exp = 60
+verified = True
+
+xp = 70
+xp = 80
+
+print("") 
+print(xp)    # Resultado: 80
+print("") 
+
+int(year = 2024)
+int(age = 24)
+
+float(pi = 3.14159)
+float(meal_cost = 12.99)
+
+str(message = "Buenas noches")
+str(user = "@kevinzambrano12345")
+
+Tarde_a_Clases = False
+cranky = True
